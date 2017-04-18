01128db03ebb47978c67723828cac1198cc0edb4
Mikkel Laursen <mikkel_laursen88@hotmail.com>
2017-04-18 15:21:52 +0200

    M

--- app/src/main/java/p2/b230/caffeinetrackerp2/MainLoggingActivity.java
+++ app/src/main/java/p2/b230/caffeinetrackerp2/MainLoggingActivity.java
@@ -3,32 +3,32 @@ package p2.b230.caffeinetrackerp2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class CalendarActivity extends AppCompatActivity {
+public class MainLoggingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_mainLogging);
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+    protected void onPause() {
+        super.onPause();
     }
 
     public void changeActivityLEFT(View view)
     {
-        Intent intent = new Intent(CalendarActivity.this, StatisticActivity.class);
+        Intent intent = new Intent(MainLoggingActivity.this, CalendarActivity.class);
         startActivity(intent);
     }
 
     public void changeActivityRIGHT(View view)
     {
-        Intent intent = new Intent(CalendarActivity.this, MainLoggingActivity.class);
+        Intent intent = new Intent(MainLoggingActivity.this, StatisticActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/p2/b230/caffeinetrackerp2/StatisticActivity.java
+++ app/src/main/java/p2/b230/caffeinetrackerp2/StatisticActivity.java
@@ -1,6 +1,7 @@
 package p2.b230.caffeinetrackerp2;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,10 +9,6 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 public class StatisticActivity extends AppCompatActivity {
 
     @Override
@@ -24,7 +21,19 @@ public class StatisticActivity extends AppCompatActivity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic);
 
     }
 
+    public void changeActivityLEFT(View view)
+    {
+        Intent intent = new Intent(StatisticActivity.this, MainLoggingActivity.class);
+        startActivity(intent);
+    }
+
+    public void changeActivityRIGHT(View view)
+    {
+        Intent intent = new Intent(StatisticActivity.this, CalendarActivity.class);
+        startActivity(intent);
+    }
 }
